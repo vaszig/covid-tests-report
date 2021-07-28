f3fd3ed41ed11b31d9f2dd90ad885d7a8a26cbef
lakis <zigras00@gmail.com>
2021-07-28 15:29:33 +0200

    fix excel file-column width

--- file_conversions.py
+++ file_conversions.py
@@ -8,6 +8,14 @@ def convert_csv_to_excel(file_path):
     Converts the csv file to an excel type of file.
     """
     csv_file = pd.read_csv(f'{file_path}.csv')
-    excel_file = pd.ExcelWriter(f'{file_path}.xlsx')
-    csv_file.to_excel(excel_file, index = False)
-    excel_file.save()
+    writer = pd.ExcelWriter(f'{file_path}.xlsx')
+    csv_file.to_excel(writer, index = False, sheet_name='report')
+
+    worksheet = writer.sheets['report']
+
+    # adjust the width of the columns
+    worksheet.set_column('A:B', 30)
+    worksheet.set_column('C:C', 20)
+    worksheet.set_column('D:E', 30)
+
+    writer.save()
